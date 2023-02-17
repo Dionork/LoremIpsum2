dbf628829fb0297a4891d2b46a60eadbf90ecd58
Dionork <rodya.ru@inbox.ru>
2023-02-17 21:56:08 +0700

    Промежуточное сохранение

---
+++ src/WordsChecker.java
@@ -0,0 +1,10 @@
+public class WordsChecker {
+    private String text;
+
+    public WordsChecker(String text) {
+        this.text = text;
+    }
+    public boolean hasWord (String word){
+        return true;
+    }
+}
