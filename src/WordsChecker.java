1f7dfe62c9074829437b2730cf1a8be0d46f19ae
Dionork <rodya.ru@inbox.ru>
2023-02-20 13:46:20 +0700

    Подделал чутка!

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -3,16 +3,14 @@ import java.util.List;
 import java.util.Set;
 
 public class WordsChecker {
-    private String text;
     private Set<String> set;
 
     public WordsChecker(String text) {
-        this.text = text;
         set = new HashSet<>();
+        set.addAll(List.of(text.split("\\P{IsAlphabetic}+")));
     }
 
     public boolean hasWord(String word) {
-        set.addAll(List.of(text.split("\\P{IsAlphabetic}+")));
         if (set.contains(word)) {
             return true;
         }
